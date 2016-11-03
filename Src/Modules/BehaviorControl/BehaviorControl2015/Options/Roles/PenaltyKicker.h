62abdfc9daad7b487d99e8917a6ab07c4a0bcf08
Trinity92 <leandro-thetrinity@hotmail.com>
2016-11-04 00:48:05 +0100

    Continued work on penalty kicker, simplified the state machine

--- Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/PenaltyKicker.h
+++ Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/PenaltyKicker.h
@@ -1,109 +1,48 @@
+/* 
+ * File:   PenaltyKicker.h
+ * Author: leandrogil
+ *
+ * Created on November 3, 2016, 1:33 PM
+ *
+ * A behaviour that represents a penalty kicker to be used during penalty kicks
+ */
+
 option(PenaltyKicker) {
-
-	initial_state(start) {
-
-		transition{
-		      if(state_time > 1000)
-		        goto turnToBall;
-		    }
-		    action{
-		      theHeadControlMode = HeadControl::lookForward;
-		      Stand();
-		    }
-	}
-
-	state(turnToBall)
-	  {
-	    transition
-	    {
-	      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
-	        goto searchForBall;
-	      if(std::abs(theBallModel.estimate.position.angle()) < 5_deg)
-	        goto walkToBall;
-	    }
-	    action
-	    {
-	      theHeadControlMode = HeadControl::lookForward;
-	      WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
-	    }
-	  }
-
-	 state(walkToBall)
-	  {
-	    transition
-	    {
-	      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
-	        goto searchForBall;
-	      if(theBallModel.estimate.position.norm() < 500.f)
-	        goto alignToGoal;
-	    }
-	    action
-	    {
-	      theHeadControlMode = HeadControl::lookForward;
-	      WalkToTarget(Pose2f(50.f, 50.f, 50.f), theBallModel.estimate.position);
-	    }
-	  }
-
-	  state(alignToGoal)
-	  {
-	    transition
-	    {
-	      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
-	        goto searchForBall;
-	      if(std::abs(libCodeRelease.angleToGoal) < 10_deg && std::abs(theBallModel.estimate.position.y()) < 100.f)
-	        goto alignBehindBall;
-	    }
-	    action
-	    {
-	      theHeadControlMode = HeadControl::lookForward;
-	      WalkToTarget(Pose2f(100.f, 100.f, 100.f), Pose2f(libCodeRelease.angleToGoal, theBallModel.estimate.position.x() - 400.f, theBallModel.estimate.position.y()));
-	    }
-	  }
-
-	  state(alignBehindBall)
-	  {
-	    transition
-	    {
-	      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
-	        goto searchForBall;
-	      if(libCodeRelease.between(theBallModel.estimate.position.y(), 20.f, 50.f)
-	          && libCodeRelease.between(theBallModel.estimate.position.x(), 140.f, 170.f)
-	          && std::abs(libCodeRelease.angleToGoal) < 2_deg)
-	        goto kick;
-	    }
-	    action
-	    {
-	      theHeadControlMode = HeadControl::lookForward;
-	      WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(libCodeRelease.angleToGoal, theBallModel.estimate.position.x() - 150.f, theBallModel.estimate.position.y() - 30.f));
-	    }
-	  }
-
-	  state(kick)
-	  {
-	    transition
-	    {
-	      if(state_time > 3000 || (state_time > 10 && action_done))
-	        goto start;
-	    }
-	    action
-	    {
-	      Annotation("Alive and Kickin'");
-	      theHeadControlMode = HeadControl::lookForward;
-	      InWalkKick(WalkRequest::left, Pose2f(libCodeRelease.angleToGoal, theBallModel.estimate.position.x() - 160.f, theBallModel.estimate.position.y() - 55.f));
-	    }
-	  }
-
-	  state(searchForBall)
-	  {
-	    transition
-	    {
-	      if(libCodeRelease.timeSinceBallWasSeen() < 300)
-	        goto turnToBall;
-	    }
-	    action
-	    {
-	      theHeadControlMode = HeadControl::lookForward;
-	      WalkAtSpeedPercentage(Pose2f(1.f, 0.f, 0.f));
-	    }
-	  }
+    initial_state(start) {
+        transition {
+            if(state_time > 1000)
+                goto alignToGoal;
+        }
+        action {
+            theHeadControlMode = HeadControl::lookForward;
+            Stand();
+        }
+    }
+    
+    state(alignToGoal) {
+        transition {
+            if(action_done)
+                goto kick;
+        }
+        action {
+            /* TODO: make the robot align to a spot on the goal that the keeper is not guarding */
+            /* TODO: test it! */
+            theHeadControlMode = HeadControl::lookForward;
+            WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
+        }
+    }
+    
+    state(kick) {
+        transition {
+            if(state_time > 3000 || (state_time > 10 && action_done))
+                goto start;
+        }
+        action {
+            Annotation("Alive and Kickin'");
+            theHeadControlMode = HeadControl::lookForward;
+            InWalkKick(WalkRequest::left, Pose2f(libCodeRelease.angleToGoal, theBallModel.estimate.position.x() - 160.f, theBallModel.estimate.position.y() - 55.f));
+        }
+    }
 }
+
+
