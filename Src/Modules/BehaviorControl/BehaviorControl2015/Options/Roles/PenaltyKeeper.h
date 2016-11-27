5fed3403fc1293fe6486ee9277ae27af9007af53
the-silent-fox <the-silent-fox@thesilentfox-X556UV>
2016-11-27 21:28:56 +0100

    Side step of the Keeper based on Y of the Kicker(to be fixed)

--- Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/PenaltyKeeper.h
+++ Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/PenaltyKeeper.h
@@ -3,8 +3,10 @@ option(PenaltyKeeper) {
 	initial_state(start) {
 		transition
 		    {
-		      if(state_time > 1000)
+		      if(state_time > 1000){
+		    	WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(250.f,0.f));
 		        goto detectShot;
+		      }
 		    }
 		    action
 		    {
@@ -16,6 +18,10 @@ option(PenaltyKeeper) {
 	state(detectShot){
 		transition
 			{
+			  if((state_time > 6700) && (theObstacleModel.obstacles[0].center(1,0) > 3.000000)) /*Shot on the right*/
+				  WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(150.f,150.f)); /*Step left*/
+			  if((state_time > 6700) && (theObstacleModel.obstacles[0].center(1,0) < -3.600000)) /*Shot on the left*/
+			     WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(-150.f,-150.f)); /*Step right*/
 			  if(libCodeRelease.shootDetected)
 				goto saveShot;
 			}
@@ -33,6 +39,10 @@ option(PenaltyKeeper) {
 			}
 		}
 		action{
+		/*If I see the Robot step aside, I move in the other direction*/
+			/*WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(-100.f,-100.f)); Step right*/
+			/*WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(100.f,100.f)); Step left*/
+
 		if(std::abs(libCodeRelease.detectedShootDirection)<3_deg)
 			SpecialAction(SpecialActionRequest::diveCenter);
 		else if (libCodeRelease.detectedShootDirection>=3_deg)
