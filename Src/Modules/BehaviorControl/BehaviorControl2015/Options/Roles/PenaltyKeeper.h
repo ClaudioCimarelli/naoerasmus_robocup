8cfacda046215ecc3a65e1c877ac515fee1d7d53
the-silent-fox <sartor.gabriele23@gmail.com>
2016-12-04 12:26:44 +0100

    PenaltyKeeper.h: commented side step behaviour

--- Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/PenaltyKeeper.h
+++ Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/PenaltyKeeper.h
@@ -18,10 +18,10 @@ option(PenaltyKeeper) {
 	state(detectShot){
 		transition
 			{
-			  if((state_time > 6700) && (theObstacleModel.obstacles[0].center(1,0) > 3.000000)) /*Shot on the right*/
-				  WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(150.f,150.f)); /*Step left*/
-			  if((state_time > 6700) && (theObstacleModel.obstacles[0].center(1,0) < -3.600000)) /*Shot on the left*/
-			     WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(-150.f,-150.f)); /*Step right*/
+//			  if((state_time > 6700) && (theObstacleModel.obstacles[0].center(1,0) > 3.000000)) /*Shot on the right*/
+//				  WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(150.f,150.f)); /*Step left*/
+//			  if((state_time > 6700) && (theObstacleModel.obstacles[0].center(1,0) < -3.600000)) /*Shot on the left*/
+//			     WalkToTarget(Pose2f(80.f, 80.f, 80.f), Pose2f(-150.f,-150.f)); /*Step right*/
 			  if(libCodeRelease.shootDetected)
 				goto saveShot;
 			}
